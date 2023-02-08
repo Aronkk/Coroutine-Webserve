2a421577039ed88d5a05744445b672fcbe67dc2c
Aron <940082524@qq.com>
2023-02-08 16:18:30 +0800

    使用cmake进行编译调试

---
+++ build/CMakeFiles/my_sylar.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lwk/Sylar/webserve/config.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/config.cc.o"
+  "/home/lwk/Sylar/webserve/fiber.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/fiber.cc.o"
+  "/home/lwk/Sylar/webserve/iomanager.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/iomanager.cc.o"
+  "/home/lwk/Sylar/webserve/log.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/log.cc.o"
+  "/home/lwk/Sylar/webserve/scheduler.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/scheduler.cc.o"
+  "/home/lwk/Sylar/webserve/thread.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/thread.cc.o"
+  "/home/lwk/Sylar/webserve/util.cc" "/home/lwk/Sylar/build/CMakeFiles/my_sylar.dir/webserve/util.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/home/lwk/apps/yaml-cpp-master/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
